0ba749a2ff945aa7259156dfac46e74fa84e2020
VINN559 <vinnalexander10@gmail.com>
2024-10-28 04:47:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const infoHeading = document.querySelector("#informasi-kelas h2");
+    infoHeading.addEventListener("click", function() {
+        alert("Informasi Kelas diklik!");
+    });
+});
